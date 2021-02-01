2fd735981091aed0aa2799ad269398d7779cd853
Alexander Nitsche <typo3@alexandernitsche.com>
2021-02-01 17:08:05 +0100

    [DOCS] TYPO3 Wiki exception images downloader

--- php/wiki.php
+++ php/wiki.php
@@ -7,101 +7,151 @@ require 'vendor/autoload.php';
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
 
-$folder = '/wiki-migration/output';
-$keepTemporaryFiles = false;
+define('WIKI_URL', 'https://wiki.typo3.org');
 
-if (!is_dir($folder)) {
-    mkdir($folder);
-}
-cleanup($folder);
-fetchExceptionPages($folder);
-convert($folder);
+// Public
+$keepTemporaryFiles = false;
+$includePages = [];
+
+// Private
+$projectDir = dirname(dirname(__FILE__));
+$outputDir = $projectDir . DIRECTORY_SEPARATOR . 'output';
+$imagesDir = $outputDir . DIRECTORY_SEPARATOR . 'Images';
+$outputUrl = '/output';
+$imagesUrl = $outputUrl . '/' . 'Images';
+$pages = [];
+$imagesMap = [];
+
+cleanDir($imagesDir);
+cleanDir($outputDir);
+createDir($outputDir);
+fetchListOfExceptionPages();
+fetchExceptionPages();
+reduceExceptionPages();
+fetchImagesOfExceptionPages();
+convert();
 if (!$keepTemporaryFiles) {
-    cleanup($folder, '/\.html$/');
+    cleanDir($outputDir, '/\.html$/');
 }
 
 /**
- * Clean up all HTML and reST files in folder.
+ * Create directory if not exists.
+ *
+ * @param string $folder
+ */
+function createDir(string $folder): void
+{
+    if (!is_dir($folder)) {
+        mkdir($folder);
+        printf("Folder %s created.\n", $folder);
+    }
+}
+
+/**
+ * Clean up all specified files in folder and remove folder if empty.
  *
  * @param string $folder
  * @param string $filePattern
  */
-function cleanup(string $folder, string $filePattern = '/(\.html|\.rst)$/'): void
+function cleanDir(string $folder, string $filePattern = ''): void
 {
-    if ($handle = opendir($folder)) {
-        while (false !== ($file = readdir($handle))) {
-            $filePath = $folder . DIRECTORY_SEPARATOR . $file;
-            if (is_file($filePath)) {
-                $pathInfo = pathinfo($filePath);
-                if (preg_match($filePattern, $pathInfo['basename'])) {
-                    unlink($filePath);
+    if (is_dir($folder)) {
+        if ($handle = opendir($folder)) {
+            while (false !== ($file = readdir($handle))) {
+                $filePath = $folder . DIRECTORY_SEPARATOR . $file;
+                if (is_file($filePath)) {
+                    $pathInfo = pathinfo($filePath);
+                    if (empty($filePattern) || preg_match($filePattern, $pathInfo['basename'])) {
+                        unlink($filePath);
+                    }
                 }
             }
+            closedir($handle);
         }
-        closedir($handle);
+        @rmdir($folder);
     }
     printf("Folder %s cleaned up.\n", $folder);
 }
 
 /**
- * Crawl TYPO3 Wiki exception pages and save their content into the specified folder.
+ * Fetch list of TYPO3 Wiki exception pages.
  *
- * @param string $folder
+ * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
  */
-function fetchExceptionPages(string $folder): void
+function fetchListOfExceptionPages(): void
 {
-    $baseUri = 'https://wiki.typo3.org';
-    $pathExceptions = 'Special:PrefixIndex?prefix=Exception&namespace=0';
+    global $includePages;
+    global $pages;
 
     $client = HttpClient::create();
-    $response = $client->request('GET', $baseUri . '/' . $pathExceptions);
+    $response = $client->request('GET', WIKI_URL . '/Special:PrefixIndex?prefix=Exception&namespace=0');
     $content = $response->getContent();
     $crawler = new Crawler($content);
-    $pathException = $crawler->filterXPath('//ul[@class="mw-prefixindex-list"]/li/a')
-        ->reduce(function(Crawler $node){return !in_array($node->text(), ['Exception']);})
-        ->each(function(Crawler $node){return $node->text();});
+    $pages = $crawler->filterXPath('//ul[@class="mw-prefixindex-list"]/li/a')
+        ->reduce(function(Crawler $node) use($includePages){
+            if (!empty($includePages)) {
+                return in_array($node->text(), $includePages);
+            } else {
+                return !in_array($node->text(), ['Exception']);
+            }
+        })
+        ->each(function(Crawler $node){
+            return $node->text();
+        });
+}
 
-    if (count($pathException) > 0) {
+/**
+ * Crawl TYPO3 Wiki exception pages and save their content into folder $outputDir.
+ */
+function fetchExceptionPages(): void
+{
+    global $pages;
+    global $outputDir;
+
+    if (count($pages) > 0) {
+        $client = HttpClient::create();
         $responses = [];
-        foreach ($pathException as $path) {
-            $responses[] = $client->request('GET', $baseUri . '/' . $path);
+        foreach ($pages as $path) {
+            $responses[] = $client->request('GET', WIKI_URL . '/' . $path);
         }
         foreach ($responses as $response) {
             $content = $response->getContent(false);
             if ($response->getStatusCode() === 200) {
                 $uri = parse_url($response->getInfo('url'));
-                $filename = explode('/', $uri['path'])[3] . '.html';
-                file_put_contents($folder . DIRECTORY_SEPARATOR . $filename, $content);
-                printf("%s fetched.\n", $response->getInfo('url'));
+                $fileName = explode('/', $uri['path'])[3] . '-s1-full.html';
+                file_put_contents($outputDir . DIRECTORY_SEPARATOR . $fileName, $content);
+                printf("Page %s fetched.\n", $response->getInfo('url'));
             } else {
-                printf("%s not fetched (status code %s)!\n", $response->getInfo('url'), $response->getStatusCode());
+                printf("Page %s not fetched (status code: %s)!\n", $response->getInfo('url'), $response->getStatusCode());
             }
         }
     }
 }
 
 /**
- * Traverse folder, extract essential html from HTML files and convert to reST files.
- *
- * @param string $folder
- * @throws Exception
+ * Traverse folder and extract essential html from HTML files.
  */
-function convert(string $folder): void
+function reduceExceptionPages(): void
 {
-    if ($handle = opendir($folder)) {
+    global $outputDir;
+
+    if ($handle = opendir($outputDir)) {
         while (false !== ($file = readdir($handle))) {
-            $filePath = $folder . DIRECTORY_SEPARATOR . $file;
+            $filePath = $outputDir . DIRECTORY_SEPARATOR . $file;
             if (is_file($filePath)) {
                 $pathInfo = pathinfo($filePath);
-                if ($pathInfo['extension'] == 'html' && strpos($pathInfo['filename'], '-extract') === false) {
+                if ($pathInfo['extension'] == 'html' && strpos($pathInfo['filename'], '-s1-full') !== false) {
+                    $pageName = str_replace('-s1-full', '', $pathInfo['filename']);
                     try {
-                        $extractFilePath = $folder . DIRECTORY_SEPARATOR . $pathInfo['filename'] . '-extract.html';
-                        $rstFilePath = $folder . DIRECTORY_SEPARATOR . $pathInfo['filename'] . '.rst';
-                        extractHtmlOfExceptionPage($filePath, $extractFilePath);
-                        convertHtmlToRst($extractFilePath, $rstFilePath);
-                        printf("%s converted.\n", $file);
+                        $targetFileName = $pageName . '-s2-reduce';
+                        $targetFilePath = $outputDir . DIRECTORY_SEPARATOR . $targetFileName . '.html';
+                        reduceExceptionPage($filePath, $targetFilePath);
+                        printf("Page %s reduced.\n", $pageName);
                     } catch (\Exception $e) {
-                        printf("%s could not be converted (%s).\n", $file, $e->getMessage());
+                        printf("Page %s could not be reduced (%s)!\n", $pageName, $e->getMessage());
                     }
                 }
             }
@@ -111,25 +161,183 @@ function convert(string $folder): void
 }
 
 /**
- * Extract essential html parts like title and content from TYPO3 Wiki Exception page,
- * e.g. from https://wiki.typo3.org/Exception/CMS/1237900529
+ * Extract essential html parts like title and content from TYPO3 Wiki Exception page
+ * and remove superfluous HTML attributes.
  *
- * @param $sourceFile
- * @param $targetFile
+ * @param string $sourceFile
+ * @param string $targetFile
  */
-function extractHtmlOfExceptionPage($sourceFile, $targetFile): void
+function reduceExceptionPage(string $sourceFile, string $targetFile): void
 {
     $crawler = new Crawler(file_get_contents($sourceFile));
     $title = $crawler->filterXPath('//h1[@id="firstHeading"]')->outerHtml();
     $content = $crawler->filterXPath('//div[@class="mw-parser-output"]/*[not(contains(@class, "toc"))]')
         ->each(function(Crawler $node){return $node->outerHtml();});
 
+    $content = preg_replace('/class="[^"]*"/', '', $content);
+    $content = preg_replace('/width="[^"]*"/', '', $content);
+    $content = preg_replace('/height="[^"]*"/', '', $content);
+
     file_put_contents($targetFile, $title . "\n\n" . implode("\n\n", $content));
 }
 
 /**
- * Convert HTML file to reST file by using external tool Pandoc,
- * see https://pandoc.org/
+ * Crawl TYPO3 Wiki exception pages and save their images into the folder $imagesDir.
+ *
+ * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+ */
+function fetchImagesOfExceptionPages(): void
+{
+    global $outputDir;
+
+    if ($handle = opendir($outputDir)) {
+        while (false !== ($file = readdir($handle))) {
+            $filePath = $outputDir . DIRECTORY_SEPARATOR . $file;
+            if (is_file($filePath)) {
+                $pathInfo = pathinfo($filePath);
+                if ($pathInfo['extension'] == 'html' && strpos($pathInfo['filename'], '-s2-reduce') !== false) {
+                    $pageName = str_replace('-s2-reduce', '', $pathInfo['filename']);
+                    try {
+                        $targetFileName = $pageName . '-s3-images-fetched';
+                        $targetFilePath = $outputDir . DIRECTORY_SEPARATOR . $targetFileName . '.html';
+                        fetchImagesOfExceptionPage($filePath, $targetFilePath, $pageName);
+                    } catch (\Exception $e) {
+                        printf("Images of page %s could not be fetched (%s)!\n", $pageName, $e->getMessage());
+                    }
+                }
+            }
+        }
+        closedir($handle);
+    }
+}
+
+/**
+ * Crawl TYPO3 Wiki exception page and save its images into the folder $imagesDir.
+ *
+ * @param string $sourceFile Exception page content with TYPO3 Wiki image urls
+ * @param string $targetFile Exception page content with local image urls
+ * @param string $pageName Exception page name
+ * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+ * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+ */
+function fetchImagesOfExceptionPage(string $sourceFile, string $targetFile, string $pageName): void
+{
+    global $imagesDir;
+    global $imagesUrl;
+    global $imagesMap;
+
+    $content = file_get_contents($sourceFile);
+    $crawler = new Crawler($content);
+    $images = $crawler->filterXPath('//img')
+        ->each(function(Crawler $node){return $node->attr('src');});
+
+    if (count($images) > 0) {
+        $client = HttpClient::create();
+        $replace = [];
+        $responses = [];
+
+        foreach ($images as $imageSrc) {
+            $imageSrcAbs = strpos($imageSrc, 'http') === 0 ? $imageSrc :
+                            (strpos($imageSrc, '/') === 0 ? WIKI_URL . $imageSrc :
+                            WIKI_URL . '/' . $imageSrc);
+
+            if (strpos($imageSrcAbs, WIKI_URL) !== false) {
+                if (!array_key_exists($imageSrc, $replace)) {
+                    $imageSrcPath = parse_url($imageSrcAbs)['path'];
+                    $imageUrl = getOriginalImageFromPotentialThumbnail($imageSrcAbs);
+                    $imageNameAndExt = pathinfo(parse_url($imageUrl)['path'])['basename'];
+                    $localImageUrl = $imagesUrl . '/' . $imageNameAndExt;
+                    $localImagePath = $imagesDir . DIRECTORY_SEPARATOR . $imageNameAndExt;
+                    $replace[$imageSrcAbs] = $localImageUrl;
+                    $replace[$imageSrcPath] = $localImageUrl;
+                    $replace[substr($imageSrcPath, 1)] = $localImageUrl;
+                    if (!array_key_exists($imageUrl, $imagesMap)) {
+                        $imagesMap[$imageUrl] = $localImagePath;
+                        $responses[] = $client->request('GET', $imageUrl);
+                    }
+                }
+            } else {
+                printf("Image %s of page %s is out of wiki scope and not fetched.\n", $imageSrc, $pageName);
+            }
+        }
+
+        if (count($responses) > 0) {
+            foreach ($responses as $response) {
+                $imageContent = $response->getContent(false);
+                $imageUrl = $response->getInfo('url');
+                if ($response->getStatusCode() === 200) {
+                    createDir($imagesDir);
+                    file_put_contents($imagesMap[$imageUrl], $imageContent);
+                    printf("Image %s of page %s fetched.\n", $imageUrl, $pageName);
+                } else {
+                    printf("Image %s of page %s not fetched (status code: %s)!\n", $imageUrl, $pageName, $response->getStatusCode());
+                }
+            }
+        }
+
+        if (count($replace) > 0) {
+            $content = str_replace(array_keys($replace), array_values($replace), $content);
+        }
+    }
+
+    file_put_contents($targetFile, $content);
+}
+
+/**
+ * Extract the original image path from a potential TYPO3 Wiki thumbnail path, e.g.
+ * https://wiki.typo3.org/wiki/images/thumb/d/d1/Extension_Upload_screen_TER.png/300px-Extension_Upload_screen_TER.png
+ * =>
+ * https://wiki.typo3.org/wiki/images/d/d1/Extension_Upload_screen_TER.png
+ *
+ * @param string $sourceFile Potential thumbnail image path
+ * @return string Original image path
+ */
+function getOriginalImageFromPotentialThumbnail(string $sourceFile): string
+{
+    $targetFile = $sourceFile;
+    if (strpos($sourceFile, '/thumb/') !== false) {
+        $targetFile = str_replace('/thumb', '', $targetFile);
+        $targetFile = substr($targetFile, 0, strrpos($targetFile, '/'));
+    }
+    return $targetFile;
+}
+
+/**
+ * Traverse folder and convert files from HTML to reST.
+ */
+function convert(): void
+{
+    global $outputDir;
+
+    if ($handle = opendir($outputDir)) {
+        while (false !== ($file = readdir($handle))) {
+            $filePath = $outputDir . DIRECTORY_SEPARATOR . $file;
+            if (is_file($filePath)) {
+                $pathInfo = pathinfo($filePath);
+                if ($pathInfo['extension'] == 'html' && strpos($pathInfo['filename'], '-s3-images-fetched') !== false) {
+                    $pageName = str_replace('-s3-images-fetched', '', $pathInfo['filename']);
+                    try {
+                        $targetFileName = $pageName;
+                        $targetFilePath = $outputDir . DIRECTORY_SEPARATOR . $targetFileName . '.rst';
+                        convertHtmlToRst($filePath, $targetFilePath);
+                        printf("Page %s converted.\n", $pageName);
+                    } catch (\Exception $e) {
+                        printf("Page %s could not be converted (%s)!\n", $pageName, $e->getMessage());
+                    }
+                }
+            }
+        }
+        closedir($handle);
+    }
+}
+
+/**
+ * Convert HTML file to reST file by using the external tool Pandoc - see https://pandoc.org/.
  *
  * @param $sourceFile
  * @param $targetFile
