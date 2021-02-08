d4dc7b8a44cccbd031a6f7d618fe44a7b500343d
Alexander Nitsche <typo3@alexandernitsche.com>
2021-02-08 14:22:55 +0100

    [TASK] Change exception title to "TYPO3 Exception 123456789"

--- php/packages/wiki/src/WikiExceptions.php
+++ php/packages/wiki/src/WikiExceptions.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Typo3\Wiki;
 
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
@@ -74,6 +75,37 @@ class WikiExceptions extends AbstractWiki
      */
     protected function getPageId(string $pageName): string
     {
-        return 'exception-' . parent::getPageId($pageName);
+        return 'typo3-exception-' . parent::getPageId($pageName);
+    }
+
+    /**
+     * Extract essential html parts like title and content from TYPO3 Wiki page
+     * and remove superfluous HTML attributes.
+     *
+     * @param string $sourceFile
+     * @param string $targetFile
+     * @param string $pageName
+     */
+    protected function reducePage(string $sourceFile, string $targetFile, string $pageName): void
+    {
+        $crawler = new Crawler(file_get_contents($sourceFile));
+        $title = sprintf('<h1>TYPO3 Exception %s</h1>', $pageName);
+        $body = $crawler->filterXPath('//div[@class="mw-parser-output"]/*[not(contains(@class, "toc"))]')
+            ->each(function(Crawler $node){return $node->outerHtml();});
+        $pageId = $this->getPageId($pageName);
+
+        $content = $title . "\n\n" . implode("\n\n", $body);
+        $content = preg_replace('/id="[^"]*"/', '', $content);
+        $content = preg_replace('/class="[^"]*"/', '', $content);
+        $content = preg_replace('/width="[^"]*"/', '', $content);
+        $content = preg_replace('/height="[^"]*"/', '', $content);
+        $content = preg_replace('/<a[^>]*>\s*<\/a>/', '', $content);
+        $content = preg_replace('/<p[^>]*>\s*<br>\s*<\/p>/', '', $content);
+
+        $content = preg_replace_callback('/<(h1[^>]*)>(.*)<\/h1>/', function($matches) use($pageId) {
+            return sprintf('<%s id="%s">%s</h1>', $matches[1], $pageId, $matches[2]);
+        }, $content, 1);
+
+        file_put_contents($targetFile, $content);
     }
 }
